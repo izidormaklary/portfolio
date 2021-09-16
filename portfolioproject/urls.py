9cf4d753d95be4096a7073d5b6514e56487cb902
izidormaklary <maklaryizidor@gmail.com>
2021-09-16 16:42:00 +0200

    crawler optimalisations

--- portfolioproject/urls.py
+++ portfolioproject/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.shortcuts import render
-
 from django.urls import include, path, re_path
+from django.views.generic import TemplateView
 
 
 def render_react(request):
@@ -11,5 +11,9 @@ def render_react(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
     re_path("", render_react)
 ]
